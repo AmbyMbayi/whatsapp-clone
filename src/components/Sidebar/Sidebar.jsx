80bbcb18c735cbc73630241c18efb617a03995c0
AmbyMbayi <ambymbayi54@gmail.com>
2020-09-21 19:55:52 +0300

    all components added

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Sidebar = () => {
+  return <h1>SidebarChat</h1>;
+};
+export default Sidebar;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Login } from "./Login/Login";
+export { default as Chat } from "./Chat/Chat";
+export { default as SidebarChat } from "./SidebarChat/SidebarChat";
+export { default as Sidebar } from "./Sidebar/Sidebar";
