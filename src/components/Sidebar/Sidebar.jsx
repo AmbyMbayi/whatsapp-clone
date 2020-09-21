d4e9e5fd92718e39cfeb3ee1bae84716fdccfe6d
AmbyMbayi <ambymbayi54@gmail.com>
2020-09-21 20:42:03 +0300

    sidebar css module done

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,42 @@
 import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVetIcon from "@material-ui/icons/MoreVert";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+
 import styles from "./Sidebar.module.css";
+import SidebarChat from "../SidebarChat/SidebarChat";
+
 const Sidebar = () => {
-  return <h1>SidebarChat</h1>;
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.sidebar_header}>
+        <Avatar />
+
+        <div className={styles.sidebar_headerRight}>
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+
+          <IconButton>
+            <MoreVetIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className={styles.sidebar_search}>
+        <div className={styles.sidebar_searchContainer}>
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="search or start a new chat..." />
+        </div>
+      </div>
+      <div className={styles.sidebar_chats}>
+        <SidebarChat />
+      </div>
+    </div>
+  );
 };
 export default Sidebar;
